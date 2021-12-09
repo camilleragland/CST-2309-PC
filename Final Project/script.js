cee6ef4a968e40c20c803109f30b6462a467a832
Camilla <camille.ragland@gmail.com>
2021-12-08 22:05:59 -0500

    finished the project

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,8 +1,26 @@
 
-
 function clicked(e)
 {
     if(!confirm('Are you sure?')) {
         e.preventDefault();
     }
+}
+var slideIndex = 0;
+carousel();
+
+function carousel(){
+var i;
+let sliderPics = document.querySelectorAll('.slide');
+
+for(i=0; i<sliderPics.length;i++){
+    sliderPics[i].style.display="none";
+}
+
+slideIndex++;
+if(slideIndex>sliderPics){
+    slideIndex=1
+}
+
+sliderPics[slideIndex-1].style.display="block";
+setTimeout(carousel,2000);
 }
